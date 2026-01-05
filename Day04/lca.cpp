65ad4e17255cca695953becf59c895e8d2a980c0
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-05 09:19:15 +0530

    Implement lowestCommonAncestor function in lca.cpp

---
+++ Day04/lca.cpp
@@ -0,0 +1,7 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(!root || root==p || root==q) return root;
+    auto l = lowestCommonAncestor(root->left,p,q);
+    auto r = lowestCommonAncestor(root->right,p,q);
+    if(l && r) return root;
+    return l?l:r;
+}
