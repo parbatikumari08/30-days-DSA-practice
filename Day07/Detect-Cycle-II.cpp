129c24cb9c85530e2116d0d58dbf1185bb143a3e
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-08 22:08:14 +0530

    Add cycle detection in linked list
    Implement Floyd's Tortoise and Hare algorithm to detect a cycle in a linked list.

---
+++ Day07/Detect-Cycle-II.cpp
@@ -0,0 +1,21 @@
+struct ListNode {
+    int val;
+    ListNode* next;
+};
+
+ListNode* detectCycle(ListNode* head) {
+    ListNode *slow = head, *fast = head;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return nullptr;
+}
