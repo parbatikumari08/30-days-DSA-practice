ba13a1ae62eb506e276dd45aeed1f3b4d02d7b87
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-10 22:16:43 +0530

    Implement daily Temperatures function using stack

---
+++ Day09/Daily-Temperatures.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int>& t) {
+    int n = t.size();
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i = n - 1; i >= 0; i--){
+        while(!st.empty() && t[st.top()] <= t[i]) st.pop();
+        ans[i] = st.empty() ? 0 : st.top() - i;
+        st.push(i);
+    }
+    return ans;
+}
