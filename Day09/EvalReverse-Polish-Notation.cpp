90e7a43275bd801de4cea324992103b03d941287
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-10 22:18:48 +0530

    Implement Reverse Polish Notation evaluator

---
+++ Day09/EvalReverse-Polish-Notation.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+int evalRPN(vector<string>& t) {
+    stack<int> st;
+    for(auto &s : t){
+        if(s=="+" || s=="-" || s=="*" || s=="/"){
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+            if(s=="+") st.push(a+b);
+            else if(s=="-") st.push(a-b);
+            else if(s=="*") st.push(a*b);
+            else st.push(a/b);
+        } else {
+            st.push(stoi(s));
+        }
+    }
+    return st.top();
+}
