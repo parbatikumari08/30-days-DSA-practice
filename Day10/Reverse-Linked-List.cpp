03c2e69ef8623f3bc5f92b47c1594383896e0d49
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-14 10:09:05 +0530

    Reverse-Linked-List.cpp

---
+++ Day10/Reverse-Linked-List.cpp
@@ -0,0 +1,12 @@
+struct ListNode { int val; ListNode* next; };
+
+ListNode* reverseList(ListNode* head) {
+    ListNode *prev = nullptr, *cur = head;
+    while(cur){
+        ListNode* nxt = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nxt;
+    }
+    return prev;
+}
