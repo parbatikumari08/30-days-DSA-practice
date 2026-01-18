de1ce4e7f67a22edc0dee95f865d7bd6c8717bdf
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-18 20:02:41 +0530

    Create Detec- Cycle.cpp

---
+++ Day11/Detec- Cycle.cpp
@@ -0,0 +1,11 @@
+struct ListNode { int val; ListNode* next; };
+
+bool hasCycle(ListNode *head) {
+    ListNode *slow=head, *fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast) return true;
+    }
+    return false;
+}
