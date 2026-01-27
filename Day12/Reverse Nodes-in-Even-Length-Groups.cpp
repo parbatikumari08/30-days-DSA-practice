71bb52bf8fb186aa73ed9e957a159c31b9a6e6c3
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-01-27 22:15:14 +0530

    Implement function to reverse nodes in even-length groups

---
+++ Day12/Reverse Nodes-in-Even-Length-Groups.cpp
@@ -0,0 +1,34 @@
+struct ListNode { int val; ListNode* next; };
+
+ListNode* reverseEvenLengthGroups(ListNode* head) {
+    ListNode dummy(0); dummy.next=head;
+    ListNode* prev=&dummy; int size=1;
+
+    while(head){
+        ListNode* cur=head;
+        int cnt=0;
+
+        while(cur && cnt<size) cur=cur->next, cnt++;
+
+        if(cnt%2==0){
+            ListNode* rev=cur;
+            ListNode* tmp=head;
+            for(int i=0;i<cnt;i++){
+                ListNode* nxt=tmp->next;
+                tmp->next=rev;
+                rev=tmp;
+                tmp=nxt;
+            }
+            prev->next=rev;
+            prev=head;
+            head=cur;
+        }else{
+            for(int i=0;i<cnt;i++){
+                prev=head;
+                head=head->next;
+            }
+        }
+        size++;
+    }
+    return dummy.next;
+}
