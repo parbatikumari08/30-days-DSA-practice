8b428be7d695186a0f87c1df6a6eccf8d4f948e8
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-02-01 23:46:42 +0530

    Implement function to find intersection of two linked lists

---
+++ Day14/Intersection-of-Two-Linked-Lists.cpp
@@ -0,0 +1,14 @@
+#include <unordered_set>
+using namespace std;
+
+struct ListNode { int val; ListNode* next; };
+
+ListNode* getIntersectionNode(ListNode* a, ListNode* b) {
+    unordered_set<ListNode*> st;
+    while(a){ st.insert(a); a = a->next; }
+    while(b){
+        if(st.count(b)) return b;
+        b = b->next;
+    }
+    return nullptr;
+}
