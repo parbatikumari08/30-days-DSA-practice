9f75555f0e2ffea27b6fdc6d802fbad5e5846340
Parbati Kumari <157682536+parbatikumari08@users.noreply.github.com>
2026-02-01 23:42:05 +0530

    Implement mergeKLists to merge k sorted linked lists

---
+++ Day13/Merge-K-orted-Linked-Lists.cpp
@@ -0,0 +1,21 @@
+#include <queue>
+using namespace std;
+
+struct ListNode { int val; ListNode* next; };
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    auto cmp = [](ListNode* a, ListNode* b){ return a->val > b->val; };
+    priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+
+    for(auto l : lists) if(l) pq.push(l);
+
+    ListNode dummy(0), *cur = &dummy;
+
+    while(!pq.empty()){
+        ListNode* node = pq.top(); pq.pop();
+        cur->next = node;
+        cur = cur->next;
+        if(node->next) pq.push(node->next);
+    }
+    return dummy.next;
+}
